f7f1b379ab1990e7048fbb5efbd62875624c6a60
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-21 21:13:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var isDate = function (input) {
   //   write your code here
+	return !isNaN[date.parse(input)];
 };
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
-alert(isDate(input));
+//alert(isDate(input));
